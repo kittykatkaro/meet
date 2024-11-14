eb891ec610f9e93b018f3fe2463fd327b4d3b38a
Karo <karoogorek@gmail.com>
2024-11-14 16:02:16 +0100

    Update EventList component to render events and add Event component

---
+++ src/components/Event.js
@@ -0,0 +1,7 @@
+// src/components/Event.js
+import React from 'react';
+const Event = () => {
+	return <li></li>;
+};
+
+export default Event;
--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,7 +1,15 @@
 // src/components/EventList.js
 import React from 'react';
-const EventList = () => {
-	return <ul id="event-list"></ul>;
+import Event from './Event';
+
+const EventList = ({ events }) => {
+	return (
+		<ul id="event-list">
+			{events
+				? events.map((event) => <Event key={event.id} event={event} />)
+				: null}
+		</ul>
+	);
 };
 
 export default EventList;
