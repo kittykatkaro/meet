9cd8e824b1f92f771ae3e0d38fc2853b25ea44d6
Karo <karoogorek@gmail.com>
2024-11-14 09:51:38 +0100

    Updates

---
+++ precache-manifest.86291229cea7c6bc3f28e47ca2028acb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cfdea7e5c57c711f88f67d66ad5b8e",
+    "url": "/meet/index.html"
+  },
+  {
+    "revision": "bc3bcfac1f63a7f4222a",
+    "url": "/meet/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "5d164cc3624bb3112780",
+    "url": "/meet/static/js/2.113d1750.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/meet/static/js/2.113d1750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a673281fd39fd0ca6b",
+    "url": "/meet/static/js/3.888f7a1d.chunk.js"
+  },
+  {
+    "revision": "bc3bcfac1f63a7f4222a",
+    "url": "/meet/static/js/main.0f49a339.chunk.js"
+  },
+  {
+    "revision": "7ccc5b22055c29623d1d",
+    "url": "/meet/static/js/runtime-main.125ac6dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meet/static/media/logo.5d5d9eef.svg"
+  }
+]);
