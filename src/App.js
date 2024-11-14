2dce2652f04fee7c85d1e706b6fc572b764a3654
Karo <karoogorek@gmail.com>
2024-11-14 14:29:25 +0100

    Refactor App component to remove header and event list placeholder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<h1>Meet App</h1>
-			<div id="event-list">{/* Your actual events goes here */}</div>
-		</div>
-	);
+	return <div className="App"></div>;
 }
 
 export default App;
