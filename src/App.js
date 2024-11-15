d62aa906dc7ed97d7ff93d9013afb6253a8aa3ec
Karo <karoogorek@gmail.com>
2024-11-15 15:26:36 +0100

    Add CitySearch component to App for improved event filtering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // src/App.js
 import React from 'react';
+import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import './App.css';
 
 const App = () => {
 	return (
 		<div className="App">
+			<CitySearch />
 			<EventList />
 		</div>
 	);
