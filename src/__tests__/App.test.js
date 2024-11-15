9d75d64009a1e7e97a9acb51be560718dc6ef596
Karo <karoogorek@gmail.com>
2024-11-15 15:55:15 +0100

    Refactor App tests to use beforeEach for rendering and add CitySearch tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,28 +2,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
-import EventList from '../components/EventList';
-import CitySearch from '../components/CitySearch';
 
 describe('<App /> component', () => {
+	let AppDOM;
+	beforeEach(() => {
+		AppDOM = render(<App />).container.firstChild;
+	});
+
 	test('renders list of events', () => {
-		const AppDOM = render(<App />).container.firstChild;
 		expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
 	});
 
-	describe('<EventList /> component', () => {
-		test('renders correct number of events', () => {
-			const EventListComponent = render(
-				<EventList
-					events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
-				/>
-			);
-			expect(EventListComponent.getAllByRole('listitem')).toHaveLength(4);
-		});
-
-		test('render CitySearch', () => {
-			const AppDOM = render(<App />).container.firstChild;
-			expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
-		});
+	test('render CitySearch', () => {
+		expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
 	});
 });
