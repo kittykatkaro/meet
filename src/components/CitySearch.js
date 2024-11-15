9163b2723af2c9384ef31ef5a0a650e5578f7a3e
Karo <karoogorek@gmail.com>
2024-11-15 16:39:05 +0100

    Implement CitySearch suggestions feature and add corresponding tests

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,14 +1,18 @@
 // src/components/CitySearch.js
 import React from 'react';
+import { useState } from 'react';
 
 const CitySearch = () => {
+	const [showSuggestions, setShowSuggestions] = useState(false);
 	return (
 		<div id="city-search">
 			<input
 				type="text"
 				className="city"
 				placeholder="Search for a city"
+				onFocus={() => setShowSuggestions(true)}
 			/>
+			{showSuggestions ? <ul className="suggestions"></ul> : null}
 		</div>
 	);
 };
