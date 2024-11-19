dd7766734441f513a713eb8b23580e3ac43d98b4
Karo <karoogorek@gmail.com>
2024-11-19 12:59:42 +0100

    Implement event fetching and location extraction in App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 // src/App.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import { extractLocations, getEvents } from './api';
 import './App.css';
+import NumberOfEvents from './components/NumberOfEvents';
 
 const App = () => {
+	const [events, setEvents] = React.useState([]);
+	const [allLocations, setAllLocations] = React.useState([]);
+
+	useEffect(() => {
+		getEvents().then((events) => {
+			const locations = extractLocations(events);
+			setAllLocations(locations);
+			setEvents(events);
+		});
+	}, []);
+
 	return (
 		<div className="App">
-			<CitySearch />
-			<EventList />
+			<CitySearch allLocations={allLocations} />
+			<NumberOfEvents />
+			<EventList events={events} />
 		</div>
 	);
 };
