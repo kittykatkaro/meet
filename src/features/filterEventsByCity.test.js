90586ebef55b1cc256a42b6f5e8e141ba70afcf6
Karo <karoogorek@gmail.com>
2024-11-26 11:16:58 +0100

    Enhance event filtering tests by adding rendering and user interaction for city search

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,4 +1,8 @@
+/* eslint-disable testing-library/no-node-access */
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import { render, waitFor, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
@@ -10,9 +14,24 @@ defineFeature(feature, (test) => {
 	}) => {
 		given('user hasn’t searched for any city', () => {});
 
-		when('the user opens the app', () => {});
+		let AppComponent;
+		when('the user opens the app', () => {
+			AppComponent = render(<App />);
+		});
 
-		then('the user should see the list of all upcoming events.', () => {});
+		then(
+			'the user should see the list of all upcoming events.',
+			async () => {
+				const AppDOM = AppComponent.container.firstChild;
+				const EventListDOM = AppDOM.querySelector('#event-list');
+
+				await waitFor(() => {
+					const EventListItems =
+						within(EventListDOM).queryAllByRole('listitem');
+					expect(EventListItems.length).toBe(35);
+				});
+			}
+		);
 	});
 
 	test('User should see a list of suggestions when they search for a city.', ({
@@ -20,9 +39,19 @@ defineFeature(feature, (test) => {
 		when,
 		then,
 	}) => {
-		given('the main page is open', () => {});
-
-		when('user starts typing in the city textbox', () => {});
+		let AppComponent;
+		given('the main page is open', () => {
+			AppComponent = render(<App />);
+		});
+		let CitySearchDOM;
+		when('user starts typing in the city textbox', async () => {
+			const user = userEvent.setup();
+			const AppDOM = AppComponent.container.firstChild;
+			CitySearchDOM = AppDOM.querySelector('#city-search');
+			const citySearchInput =
+				within(CitySearchDOM).queryByRole('textbox');
+			await user.type(citySearchInput, 'Berlin');
+		});
 
 		then(
 			'the user should recieve a list of cities (suggestions) that match what they’ve typed',
