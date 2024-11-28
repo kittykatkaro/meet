19a940b646f2d1cabaa9e0409e9250da0a04f500
Karo <karoogorek@gmail.com>
2024-11-28 15:52:53 +0100

    Add documentation for extractLocations and getEvents functions in api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,18 @@
-// src/api.js
-
 import mockData from './mock-data';
 
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ *  */
 export const extractLocations = (events) => {
 	const extractedLocations = events.map((event) => event.location);
 	const locations = [...new Set(extractedLocations)];
 	return locations;
 };
-
 const checkToken = async (accessToken) => {
 	const response = await fetch(
 		`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
@@ -34,7 +39,7 @@ const removeQuery = () => {
 const getToken = async (code) => {
 	const encodeCode = encodeURIComponent(code);
 	const response = await fetch(
-		'https://py1asmcx3g.execute-api.eu-central-1.amazonaws.com/dev/api/token/' +
+		'https://py1asmcx3g.execute-api.eu-central-1.amazonaws.com/dev/api/token' +
 			'/' +
 			encodeCode
 	);
@@ -44,6 +49,10 @@ const getToken = async (code) => {
 	return access_token;
 };
 
+/**
+ *
+ * This function will fetch the list of all events
+ * */
 export const getEvents = async () => {
 	if (window.location.href.startsWith('http://localhost')) {
 		return mockData;
@@ -70,7 +79,6 @@ export const getEvents = async () => {
 		} else return null;
 	}
 };
-
 export const getAccessToken = async () => {
 	const accessToken = localStorage.getItem('access_token');
 	const tokenCheck = accessToken && (await checkToken(accessToken));
