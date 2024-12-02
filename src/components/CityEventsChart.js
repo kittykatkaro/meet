3e083e6ec0db6215655bf6237bcf182cd322bae8
Karo <karoogorek@gmail.com>
2024-12-02 14:42:02 +0100

    Update CityEventsChart to improve data handling and enhance chart layout

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -16,14 +16,14 @@ const CityEventsChart = ({ allLocations, events }) => {
 
 	useEffect(() => {
 		setData(getData());
-	}, [`${data}`]);
+	}, [`${events}`]);
 
 	const getData = () => {
 		const data = allLocations.map((location) => {
 			const count = events.filter(
 				(event) => event.location === location
 			).length;
-			const city = location.split(', ')[0];
+			const city = location.split(/, | - /)[0];
 			return { city, count };
 		});
 		return data;
@@ -33,17 +33,24 @@ const CityEventsChart = ({ allLocations, events }) => {
 		<ResponsiveContainer width="99%" height={400}>
 			<ScatterChart
 				margin={{
-					top: 20,
+					top: 30,
 					right: 20,
-					bottom: 20,
-					left: 20,
+					bottom: 60,
+					left: -30,
 				}}
 			>
 				<CartesianGrid />
-				<XAxis type="category" dataKey="city" name="City" />
+				<XAxis
+					type="category"
+					dataKey="city"
+					name="City"
+					angle={60}
+					interval={0}
+					tick={{ dx: 20, dy: 40, fontSize: 14 }}
+				/>
 				<YAxis type="number" dataKey="count" name="Number of events" />
 				<Tooltip cursor={{ strokeDasharray: '3 3' }} />
-				<Scatter name="A school" data={data} fill="#8884d8" />
+				<Scatter name="Locations" data={data} fill="#8884d8" />
 			</ScatterChart>
 		</ResponsiveContainer>
 	);
